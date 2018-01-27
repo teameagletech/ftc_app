d83b7c8b1bb1901ada771cb72d57b8303946bffe
IshanArya <ishan.arya130@gmail.com>
2018-01-26 23:07:26 -0500

    Code revamp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EchoAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EchoAutonomous.java
@@ -9,35 +9,14 @@ import com.qualcomm.robotcore.hardware.Servo;
 @Autonomous(name = "EchoAutonomous")
 public class EchoAutonomous extends LinearOpMode {
 
+    CommonMotions commonMotions;
 
-    private DcMotor driveLeftFront, driveLeftBack, driveRightFront, driveRightBack;
-    private DcMotor elevator, hand;
-    private Servo pusher;
 
-    private int pullMotorStart1 = 1000;
-
-    private int handMaxPosition = 163;
-    private int handMinPosition = 0;
+    private DcMotor elevator;
 
     public void initRobot() {
-        driveLeftFront = hardwareMap.dcMotor.get("dlf");
-        driveLeftBack = hardwareMap.dcMotor.get("dlb");
-        driveRightFront = hardwareMap.dcMotor.get("drf");
-        driveRightBack = hardwareMap.dcMotor.get("drb");
-
         elevator = hardwareMap.dcMotor.get("elevator");
-        hand = hardwareMap.dcMotor.get("hand");
-
-        pusher = hardwareMap.servo.get("pusher");
-
-        driveLeftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        driveLeftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        driveRightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        driveRightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        elevator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        hand.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        pusher.setPosition(1);
+        commonMotions = new CommonMotions(hardwareMap, true);
     }
 
 
@@ -48,61 +27,49 @@ public class EchoAutonomous extends LinearOpMode {
         telemetry.addData("Message", "Don't lose too hard.");
 
         waitForStart();
-        closeHand();
+        commonMotions.closeHand();
 
-        elevator.setTargetPosition(pullMotorStart1);
+        elevator.setTargetPosition(commonMotions.elevatorStart2);
         elevator.setPower(1);
+        Thread.sleep(2000);
 
 //        driveLeftFront.setPower(backwardsMode * gamepad1.left_stick_y * speedModifier);
 //        driveLeftBack.setPower(backwardsMode * -gamepad1.left_stick_y * speedModifier);
 //        driveRightFront.setPower(backwardsMode * -gamepad1.right_stick_y * speedModifier);
 //        driveRightBack.setPower(backwardsMode * gamepad1.right_stick_y * speedModifier);
 
-        driveLeftFront.setPower(-1);
-        driveLeftBack.setPower(1);
-        driveRightFront.setPower(1);
-        driveRightBack.setPower(-1);
+        commonMotions.drive(1, 0, 0);
         Thread.sleep(2000);
-        driveLeftFront.setPower(0);
-        driveLeftBack.setPower(0);
-        driveRightFront.setPower(0);
-        driveRightBack.setPower(0);
+        commonMotions.stopWheels();
 
-        pusher.setPosition(0.55);
+        commonMotions.pusherPush();
 
-        openHand();
+        commonMotions.openHand();
+        Thread.sleep(2000);
 
-        driveLeftFront.setPower(0.5);
-        driveLeftBack.setPower(-0.5);
-        driveRightFront.setPower(-0.5);
-        driveRightBack.setPower(0.5);
+        commonMotions.drive(-1, 0, 0);
 
-        Thread.sleep(250);
+        Thread.sleep(500);
 
-        driveLeftFront.setPower(0);
-        driveLeftBack.setPower(0);
-        driveRightFront.setPower(0);
-        driveRightBack.setPower(0);
-        pusher.setPosition(1);
+        commonMotions.stopWheels();
+        elevator.setTargetPosition(commonMotions.elevatorMinTurn);
+        commonMotions.closeHand();
+        commonMotions.pusherClose();
+        Thread.sleep(2000);
+        commonMotions.drive(1, 0, 0);
+        Thread.sleep(750);
+        commonMotions.stopWheels();
+        Thread.sleep(2000);
+        commonMotions.drive(-1, 0, 0);
+        Thread.sleep(200);
+        commonMotions.stopWheels();
+        Thread.sleep(2000);
+        commonMotions.drive(0, 0, 1);
+        Thread.sleep(500);
+        commonMotions.stopWheels();
+        Thread.sleep(2000);
 
         stop();
-    }
-
-    private void openHand() {
-        hand.setTargetPosition(handMinPosition);
-        hand.setPower(0.5);
-    }
-
-    private void closeHand() {
-        hand.setTargetPosition(handMaxPosition);
-        hand.setPower(-0.5);
-    }
-
-    private void stopRobot() {
-        driveLeftFront.setPower(0);
-        driveLeftBack.setPower(0);
-        driveRightFront.setPower(0);
-        driveRightBack.setPower(0);
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EchoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EchoOp.java
@@ -12,46 +12,19 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "EchoOp")
 public class EchoOp extends OpMode {
 
+    CommonMotions commonMotions;
+
     private DcMotor driveLeftFront, driveLeftBack, driveRightFront, driveRightBack;
-    private DcMotor elevator, hand;
+    private DcMotor elevator, hand, relicTool;
+    private Servo relicGrabber, relicLift;
     private Servo pusher;
 
-
-    private int elevatorMaxTurn = 10000;
-    private int elevatorEnd1 = 9000;
-    private int elevatorEnd2 = 8000;
-    private int elevatorStart2 = 2000;
-    private int elevatorStart1 = 1000;
-    private int elevatorMinTurn = 0;
-
-    private int handMaxPosition = 175;
-    private int handMinPosition = 0;
-
-    private double speedModifier = 1;
-
-    private int backwardsMode = 1;
 
     @Override
     public void init() {
 
-        driveLeftFront = hardwareMap.dcMotor.get("dlf");
-        driveLeftBack = hardwareMap.dcMotor.get("dlb");
-        driveRightFront = hardwareMap.dcMotor.get("drf");
-        driveRightBack = hardwareMap.dcMotor.get("drb");
+        commonMotions = new CommonMotions(hardwareMap, false);
 
-        elevator = hardwareMap.dcMotor.get("elevator");
-        hand = hardwareMap.dcMotor.get("hand");
-        pusher = hardwareMap.servo.get("pusher");
-
-        driveLeftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        driveLeftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        driveRightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        driveRightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        elevator.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        hand.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        pusher.setPosition(1);
 
     }
 
@@ -61,105 +34,78 @@ public class EchoOp extends OpMode {
         // Controller 1
 
         if (gamepad1.y) {
-            speedModifier = 0.3;
+            commonMotions.enablePrecisionMode();
         } else if(gamepad1.x) {
-            speedModifier = 1;
-        }
-
-        if (gamepad1.b) {
-            backwardsMode = -1;
-        } else if (gamepad1.a) {
-            backwardsMode = 1;
+            commonMotions.disablePrecisionMode();
         }
 
 
-
-
-        driveLeftFront.setPower(-gamepad1.left_stick_x * speedModifier);
-        driveLeftBack.setPower(-gamepad1.left_stick_x * speedModifier);
-        driveRightBack.setPower(-gamepad1.left_stick_x * speedModifier);
-        driveRightFront.setPower(-gamepad1.left_stick_x * speedModifier);
-
-//        driveLeftFront.setPower(backwardsMode * gamepad1.left_stick_y * speedModifier);
-//        driveLeftBack.setPower(backwardsMode * -gamepad1.left_stick_y * speedModifier);
-//        driveRightFront.setPower(backwardsMode * -gamepad1.left_stick_y * speedModifier);
-//        driveRightBack.setPower(backwardsMode * gamepad1.left_stick_y * speedModifier);
-
-//        driveLeftFront.setPower(backwardsMode * gamepad1.right_stick_x * speedModifier);
-//        driveLeftBack.setPower(backwardsMode * -gamepad1.right_stick_x * speedModifier);
-//        driveRightFront.setPower(backwardsMode * gamepad1.right_stick_x * speedModifier);
-//        driveRightBack.setPower(backwardsMode * -gamepad1.right_stick_x * speedModifier);
+        commonMotions.drive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
 
         /**
          * Flap in front of robot
          */
         if(gamepad1.left_bumper) {
-            pusher.setPosition(0.55);
+            commonMotions.pusherPush();
         } else {
-            pusher.setPosition(1);
+            commonMotions.pusherClose();
         }
 
         //########################################################################################################################################################################
 
         // Controller 2
 
-        int elevatorPosition = elevator.getCurrentPosition();
         if(gamepad2.dpad_up) {
-            if(elevatorPosition < elevatorStart1) {
-                elevator.setPower(0.25);
-            } else if(elevatorPosition < elevatorStart2) {
-                elevator.setPower(0.5);
-            } else if(elevatorPosition < elevatorEnd2) {
-                elevator.setPower(1);
-            } else if(elevatorPosition < elevatorEnd1) {
-                elevator.setPower(0.5);
-            } else if(elevatorPosition < elevatorMaxTurn) {
-                elevator.setPower(0.25);
-            } else {
-                elevator.setPower(0);
-            }
+            commonMotions.elevatorAscend();
         } else if(gamepad2.dpad_down) {
-            if(elevatorPosition > elevatorEnd1) {
-                elevator.setPower(-0.25);
-            } else if(elevatorPosition > elevatorEnd2) {
-                elevator.setPower(-0.5);
-            } else if(elevatorPosition > elevatorStart2) {
-                elevator.setPower(-1);
-            } else if(elevatorPosition > elevatorStart1) {
-                elevator.setPower(-0.5);
-            } else if(elevatorPosition > elevatorMinTurn) {
-                elevator.setPower(-0.25);
-            } else {
-                elevator.setPower(0);
-            }
+            commonMotions.elevatorDescend();
         } else {
-            elevator.setPower(0);
+            commonMotions.stopElevator();
         }
 
         if(gamepad2.right_bumper) {
-            closeHand();
+            commonMotions.closeHand();
         } else {
-            openHand();
+            commonMotions.openHand();
         }
 
 
+        if(gamepad2.left_bumper) {
+            commonMotions.resetElevator();
+        }
+
+
+        commonMotions.relicToolExtend(gamepad2.left_trigger);
+
+        if(gamepad2.b) {
+            commonMotions.openRelicGrabber();
+        } else if(gamepad2.a) {
+            commonMotions.closeRelicGrabber();
+        }
         if(gamepad2.y) {
-            elevator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            elevator.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            commonMotions.relicLiftUp();
+        } else if(gamepad2.x) {
+            commonMotions.relicLiftDown();
         }
+
+
+
+
+
+
+        telemetry.addData("Gamepad 1 Left Stick X", gamepad1.left_stick_x);
+        telemetry.addData("Gamepad 1 Left Stick Y", -gamepad1.left_stick_y);
+        telemetry.addData("Gamepad 1 Right Stick X", gamepad1.right_stick_x);
+        telemetry.addData("Gamepad 2 Right Trigger", gamepad2.right_trigger);
+
 
         telemetry.addData("Elevator Position", elevator.getCurrentPosition());
         telemetry.addData("Hand Position", hand.getCurrentPosition());
-        telemetry.addData("Zero Position", elevatorMinTurn);
-    }
+        telemetry.addData("Relic Tool Power", relicTool.getPower());
+        telemetry.addData("Relic Grabber Position", relicGrabber.getPosition());
+        telemetry.addData("Relic Lift Position", relicLift.getPosition());
 
-    private void openHand() {
-        hand.setTargetPosition(handMinPosition);
-        hand.setPower(0.5);
-    }
 
-    private void closeHand() {
-        hand.setTargetPosition(handMaxPosition);
-        hand.setPower(-0.5);
+
     }
 }
